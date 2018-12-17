fd7942956fcdaec74e9d404af9146b16860b631f
Pavithra Preethi N <pavithrapreethi.n@cognizant.com>
2018-12-17 11:14:08 +0530

    Updated files and dependencies

--- welcome-service/src/main/java/com/msa/pcf/welcomeservice/WelcomeServiceApplication.java
+++ welcome-service/src/main/java/com/msa/pcf/welcomeservice/WelcomeServiceApplication.java
@@ -2,8 +2,12 @@ package com.msa.pcf.welcomeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.msa.pcf.welcomeservice")
 public class WelcomeServiceApplication {
 
 	public static void main(String[] args) {
