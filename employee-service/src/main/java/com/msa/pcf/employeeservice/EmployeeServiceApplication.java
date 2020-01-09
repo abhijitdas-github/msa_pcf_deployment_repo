ab7ebae8c18c5487c33d49627b3b1bd920acef86
abhijit.ad.das <abhijit.ad.das@IP-C6136396.corp.amazonworkspaces.com>
2020-01-09 17:10:10 +0530

    1

--- employee-service/src/main/java/com/msa/pcf/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/msa/pcf/employeeservice/EmployeeServiceApplication.java
@@ -2,10 +2,16 @@ package com.msa.pcf.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableConfigurationProperties(EmployeeConfiguration.class)
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
