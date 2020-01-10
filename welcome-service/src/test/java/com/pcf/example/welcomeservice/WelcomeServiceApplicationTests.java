c8cbe28fc00f84c4ee7e930c9c36042c6c2fe05d
abhijit.ad.das <abhijit.ad.das@IP-C6136396.corp.amazonworkspaces.com>
2020-01-10 18:26:35 +0530

    1

--- welcome-service/src/test/java/com/pcf/example/welcomeservice/WelcomeServiceApplicationTests.java
+++ welcome-service/src/test/java/com/pcf/example/welcomeservice/WelcomeServiceApplicationTests.java
@@ -1,16 +1,16 @@
-package com.pcf.example.welcomeservice;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class WelcomeServiceApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.pcf.example.welcomeservice;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class WelcomeServiceApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
